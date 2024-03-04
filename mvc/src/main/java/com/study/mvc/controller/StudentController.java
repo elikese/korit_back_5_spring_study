4b5539503b5313210dd41822d3eb8c29482197cd
elikese <elikese@gmail.com>
2024-03-04 15:51:03 +0900

    responseEntity static 메서드 사용하여 response

---
+++ mvc/src/main/java/com/study/mvc/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.study.mvc.controller;
+
+import com.study.mvc.dto.StudentReqDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController{
+
+    // 파라미터의 객체생성을 dispatcher가 요청 받은 시점에 인스턴스 생성함.
+
+//    @GetMapping("/student1")
+//    public Object getStudentInfo1(@RequestParam StudentReqDto studentReqDto) {
+//        return studentReqDto.toRespDto();
+//    }
+
+    @GetMapping("/student")
+    public ResponseEntity<?> getStudentInfo(@ModelAttribute StudentReqDto studentReqDto) {
+        // ResponseEntity -> 상태코드, 헤더설정 등 한번에 가능(메서드 체인)
+        return ResponseEntity.ok().body(studentReqDto.toRespDto());
+    }
+}
--- mvc/src/main/java/com/study/mvc/controller/StudyRestController.java
+++ mvc/src/main/java/com/study/mvc/controller/StudyRestController.java
@@ -1,13 +1,54 @@
 package com.study.mvc.controller;
 
+import com.study.mvc.dto.HelloDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 @RestController // 모든 메서드에 @ResponseBody 어노테이션을 단 것과 같다.
 public class StudyRestController {
 
-    @GetMapping("hello/test")
-    public String hello() {
-        return "Hello";
+//    ** 이전 JSP 방식
+//    @GetMapping("hello/test")
+//    public String hello(HttpServletRequest request) {
+//        System.out.println(request.getParameter("name"));
+//        return request.getParameter("name");
+//    }
+
+
+    //기본형태
+    //생략이 가능해도 명시해주는게 좋음
+    @GetMapping("hello/test1")
+    public String hello1(@RequestParam("name") String str) {
+        return str;
     }
+
+    //변수명 일치시 어노테이션 생략
+    @GetMapping("hello/test2")
+    public String hello2(String name) {
+        return name;
+    }
+
+    //어노테이션 생략하고 파라미터 2개
+    @GetMapping("hello/test3")
+    public String hello3(String name, String age) {
+        return name + " " + age;
+    }
+
+    //파라미터를 int로 받아도 알아서 파싱해서 가져옴
+    @GetMapping("hello/test4")
+    public String hello4(String name, int age) {
+        return name + " " + age;
+    }
+
+    //객체 반환시 NoArgConstructor없으면 부분적으로 null 불가능
+    //dto는 내가 직접 생성하는 경우가 거의 없음 -> @NoArg , @AllArg, @Builer 필요없음(기본갑이 NoArg생성자니까)
+    @GetMapping("hello/test5")
+    public String hello5(HelloDto helloDto) {
+        return helloDto.toString();
+    }
+
 }
