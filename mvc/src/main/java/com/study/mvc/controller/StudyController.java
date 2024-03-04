a71cdd36463489eacf241bc99162b33902855301
elikese <elikese@gmail.com>
2024-03-04 11:44:14 +0900

    mvc rest 두가지 study

--- mvc/src/main/java/com/study/mvc/controller/StudyController.java
+++ mvc/src/main/java/com/study/mvc/controller/StudyController.java
@@ -3,20 +3,34 @@ package com.study.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class StudyController {
 
+    // MVC 패턴
     @GetMapping("/hello")
-    public ModelAndView helloPage(Model model) {
-        model.addAttribute("name", "박화목");
-        return new ModelAndView("hello");
+    public ModelAndView helloPage() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("name1", "피카츄");
+        model.put("name2", "라이츄");
+        model.put("name3", "파이리");
+        return new ModelAndView("hello", model);
     }
 
+    // REST
     @GetMapping("/test")
-    public String testPage(Model model) {
-        model.addAttribute("age",33);
-        return "test";
+    @ResponseBody // @RespnseBody가 붙으면, viewname을 리턴하는게 아니고, data만 리턴해줌 -> CSR 아님.
+    public Map<String, Object> testPage(Model model) {
+        Map<String, Object> testObj = new HashMap<>();
+        testObj.put("age", 33);
+        testObj.put("피카츄1", 55);
+        testObj.put("피카츄2", model);
+        return testObj;
     }
+
 }
